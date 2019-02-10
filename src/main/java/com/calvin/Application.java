7ff9c0f3d88502854ee2b32d484564680c4e5f6b
Jiayuan <cavidasham@gmail.com>
2019-02-10 20:20:35 +0000

    add spring

--- src/main/java/com/calvin/Application.java
+++ src/main/java/com/calvin/Application.java
@@ -7,7 +7,7 @@ public class Application {
     public static void  main(String[] args){
 
         CustomerService service = new CustomerServiceImpl();
-        System.out.println(service.findAll().get(0));
+        System.out.println(service.findAll().get(0).getFirstName());
 
     }
 
